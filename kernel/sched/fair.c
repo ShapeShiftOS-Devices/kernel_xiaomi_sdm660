4833b10ca3e145875ca383bd79832b42ac626e9d
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2018-02-04 19:28:12 -0800

    sched/fair: update_rq_clock after nr_running check in load balance path
    As there is a possibility that number of running tasks could be
    less than or equal to one and the check for nr_running leads to
    not to move of the tasks, updating update_rq_clock before check
    is wrong, fix it by moving update_rq_clock after the check.
    
    Change-Id: I1719a5482af3c35131e18e9fd521cb9e5d2de2ae
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9312,7 +9312,6 @@ redo:
 
 more_balance:
 		raw_spin_lock_irqsave(&busiest->lock, flags);
-		update_rq_clock(busiest);
 
 		/* The world might have changed. Validate assumptions */
 		if (busiest->nr_running <= 1) {
@@ -9320,6 +9319,8 @@ more_balance:
 			env.flags &= ~LBF_ALL_PINNED;
 			goto no_move;
 		}
+
+		update_rq_clock(busiest);
 
 		/*
 		 * cur_ld_moved - load moved in current iteration
